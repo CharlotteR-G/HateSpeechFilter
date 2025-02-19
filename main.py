45ef3ec11bb36e6e259dbf07721e9f078e05a016
CharlotteR-G <107860202+CharlotteR-G@users.noreply.github.com>
2025-02-19 12:16:04 +0000

    CRG: Added initial pipeline for formatting code via flake8 and fixed formatiing with black

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from transformers import pipeline
 app = FastAPI()
 
 # Initialize the hate speech detection pipeline
-pipe = pipeline("text-classification", model="Hate-speech-CNERG/dehatebert-mono-english")
+pipe = pipeline(
+    "text-classification", model="Hate-speech-CNERG/dehatebert-mono-english"
+)
+
 
 @app.post("/filter_comment/")
 async def filter_comment(comment: str):
     try:
         # Get the prediction from the pipeline
         result = pipe(comment)
-        
-        if result[0]['label'] == "HATE":
+
+        if result[0]["label"] == "HATE":
             return True
         else:
             return False
