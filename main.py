2d021559ae29ab5a802dd4c29e24811fca228a38
CharlotteR-G <107860202+CharlotteR-G@users.noreply.github.com>
2025-03-05 12:43:39 +0000

    CRG: Changed implemntation of hate speech filter so python interface now deals with database

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, HTTPException
 from transformers import pipeline
-import httpx
 
 app = FastAPI()
 
@@ -9,8 +8,6 @@ pipe = pipeline(
     "text-classification", model="Hate-speech-CNERG/dehatebert-mono-english"
 )
 
-DATABASE_SERVICE_URL = "http://127.0.0.1:5000"
-
 
 @app.post("/filter")
 async def filter_comment(star_data: dict):
@@ -18,24 +15,16 @@ async def filter_comment(star_data: dict):
         # Extract the message from the star_data
         message = star_data.get("message")
         if not message:
-            raise HTTPException(status_code=400, detail="Message is required")
-
+            # No message should be dealt same as hate speech, no star
+            return {"status": False, "message": "Message was empty"}
+        
         # Get the prediction from the pipeline
         result = pipe(message)
-        print("Processed message:", message)
 
         if result[0]["label"] == "HATE":
-            return {"status": "error", "message": "Message was inappropriate"}
+            return {"status": False, "message": "Message was inappropriate"}
         else:
-            # If the message is acceptable, forward to database
-            async with httpx.AsyncClient() as client:
-                db_resp = await client.post(
-                    f"{DATABASE_SERVICE_URL}/stars", json=star_data
-                )
-            if db_resp.status_code != 200:
-                raise HTTPException(
-                    status_code=db_resp.status_code, detail=db_resp.text
-                )
-            return db_resp.json()
+            return {"status": True, "message": "Message was acceptable"}
+        
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
