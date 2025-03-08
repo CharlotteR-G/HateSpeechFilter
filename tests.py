f87ec67cff89c25a96734ea99abc84d1e167fd09
CharlotteR-G <107860202+CharlotteR-G@users.noreply.github.com>
2025-03-08 11:58:57 +0000

    CRG: fixed test formatting

--- tests.py
+++ tests.py
@@ -5,33 +5,44 @@ from main import app
 # Initialize the test client
 client = TestClient(app)
 
+
 def test_filter_comment_empty_message():
     response = client.post("/filter", json={"message": ""})
     assert response.status_code == 200
-    assert response.json() == {"status": False, "message": "Message was empty"}
+    assert response.json() == {"status": False, "message":
+                               "Message was empty"}
+
 
 def test_filter_comment_long_message():
     long_message = "a" * 256
     response = client.post("/filter", json={"message": long_message})
     assert response.status_code == 200
-    assert response.json() == {"status": False, "message": "Message was too long"}
+    assert response.json() == {"status": False, "message":
+                               "Message was too long"}
+
 
 def test_filter_comment_hateful():
     hate_message = "You are an idiot"
     response = client.post("/filter", json={"message": hate_message})
     assert response.status_code == 200
-    assert response.json() == {"status": False, "message": "Message was inappropriate"}
+    assert response.json() == {"status": False, "message":
+                               "Message was inappropriate"}
+
 
 def test_filter_comment_acceptable_message():
     acceptable_message = "Have a great day!"
     response = client.post("/filter", json={"message": acceptable_message})
     assert response.status_code == 200
-    assert response.json() == {"status": True, "message": "Message was acceptable"}
+    assert response.json() == {"status": True, "message":
+                               "Message was acceptable"}
+
 
 def test_filter_comment_invalid_input():
     response = client.post("/filter", json={"wrong_key": "This is a message"})
     assert response.status_code == 200
-    assert response.json() == {"status": False, "message": "Message was empty"}
+    assert response.json() == {"status": False, "message":
+                               "Message was empty"}
+
 
 def test_filter_comment_server_error():
     with pytest.raises(Exception):
